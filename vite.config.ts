0387ff182d59894ccb1757347ad94615f6582f93
Joey <joey.xf@gmail.com>
2024-09-19 16:55:15 +0800

    add node support

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { nodePolyfills } from "vite-plugin-node-polyfills"; // Fix Buffer is not defined
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), nodePolyfills()]
 });
