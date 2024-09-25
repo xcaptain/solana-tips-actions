65c10b21b80ccc2d00fec0dc67f346f6ecea0a1e
Joey <joey.xf@gmail.com>
2024-09-25 17:25:57 +0800

    remove vitee node polyfill

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { nodePolyfills } from "vite-plugin-node-polyfills"; // Fix Buffer is not defined
 
 export default defineConfig({
-	plugins: [sveltekit(), nodePolyfills()]
+	plugins: [sveltekit()]
 });
